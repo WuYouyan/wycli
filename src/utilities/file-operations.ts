b0db5daca664e10394ab3ebdb3aa98762a71f30d
WY <wuyouyan1@gmail.com>
2023-07-23 23:49:35 +0200

    add file operation utility functions

--- src/utilities/file-operations.ts
+++ src/utilities/file-operations.ts
@@ -1,4 +1,4 @@
-import { existsSync, mkdirSync, openSync, writeFile } from "fs";
+import { createReadStream, createWriteStream, existsSync, mkdirSync, openSync, readFile, renameSync, writeFile } from "fs";
 import { VirtualFile } from "../models/virtual-file.model";
 import chalk from "chalk";
 
@@ -34,4 +34,51 @@ export function createFile(virtualFile: VirtualFile) {
     } else {
         createFile(virtualFile.renameWhenExists());
     }
+}
+
+export function replaceString(filePath: string, target: string, replace: string) {
+    readFile(filePath, function (err, data) {
+        if (err) throw err;
+        
+        const content = data.toString();
+        const newContent = content.replace(target, replace);
+    
+        writeFile(filePath, newContent, function (err) {
+            if (err) throw err;
+            console.log('Replaced!');
+        });
+    });
+}
+export function replaceStringBigFile(filePath: string, target: string, replace: string) { 
+    const readStream = createReadStream(filePath);
+    const writeStream = createWriteStream(filePath + '.tmp');
+
+    let remaining: string = '';
+
+    readStream.on('data', function (chunk) {
+        const lines = (remaining + chunk).split(/\r?\n/g);
+        remaining = lines.pop() || "";
+
+        for (let i = 0; i < lines.length; ++i) {
+            if (lines[i].includes(target)) {
+                lines[i] = lines[i].replace(/\/\/INSERT HERE/g, replace);
+            }
+        }
+
+        writeStream.write(lines.join('\n') + '\n');
+    });
+
+    readStream.on('end', function () {
+        if (remaining.length > 0) {
+            writeStream.write(remaining);
+        }
+
+        writeStream.end();
+    });
+
+    writeStream.on('finish', function () {
+        renameSync(filePath + '.tmp', filePath);
+        console.log('Replaced!');
+    });
+
 }
