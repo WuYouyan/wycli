df3f5192c482d51d86842b2634990a669620e395
WY <wuyouyan1@gmail.com>
2023-07-24 12:05:02 +0200

    add file operation utility function

--- src/utilities/file-operations.ts
+++ src/utilities/file-operations.ts
@@ -1,6 +1,7 @@
-import { createReadStream, createWriteStream, existsSync, mkdirSync, openSync, readFile, renameSync, writeFile } from "fs";
-import { VirtualFile } from "../models/virtual-file.model";
 import chalk from "chalk";
+import { createReadStream, createWriteStream, existsSync, mkdirSync, openSync, readFile, rename, renameSync, writeFile } from "fs";
+import { Transform } from "stream";
+import { VirtualFile } from "../models/virtual-file.model";
 
 /**
  * create directory
@@ -39,17 +40,17 @@ export function createFile(virtualFile: VirtualFile) {
 export function replaceString(filePath: string, target: string, replace: string) {
     readFile(filePath, function (err, data) {
         if (err) throw err;
-        
+
         const content = data.toString();
         const newContent = content.replace(target, replace);
-    
+
         writeFile(filePath, newContent, function (err) {
             if (err) throw err;
             console.log('Replaced!');
         });
     });
 }
-export function replaceStringBigFile(filePath: string, target: string, replace: string) { 
+export function replaceStringBigFile(filePath: string, target: string, replace: string) {
     const readStream = createReadStream(filePath);
     const writeStream = createWriteStream(filePath + '.tmp');
 
@@ -75,10 +76,92 @@ export function replaceStringBigFile(filePath: string, target: string, replace: 
 
         writeStream.end();
     });
+    readStream.on('error', function (err) {
+        console.error(chalk.red(err));
+        writeStream.end();
+    });
 
     writeStream.on('finish', function () {
         renameSync(filePath + '.tmp', filePath);
         console.log('Replaced!');
     });
+    writeStream.on('error', function (err) {
+        console.error(chalk.red(err));
+        readStream.destroy();
+    });
 
+}
+/**
+ * Replaces a string matched in a file.
+ *
+ * @param {string} filePath - The path of the file.
+ * @param {string} target - The string to be replaced.
+ * @param {string} replace - The string to replace the target.
+ * @param {boolean} [replaceFirst=false] - Indicates whether to replace only the first occurrence or all occurrences of the target.
+ */
+export function replaceStringInFile(filePath: string, target: string, replace: string, replaceFirst: boolean = false) {
+    let replaced = false;
+    let remaining = '';
+    let regexTag = replaceFirst?'':'g';
+    const targetRegex = new RegExp(target, regexTag);
+    /**
+     * @see https://nodejs.org/api/stream.html
+     */
+    const transformStream = new Transform({
+        transform(chunk, encoding, callback) {
+            let data = remaining + chunk.toString();
+            if (!replaced) {
+                const index = data.indexOf(target);
+                if (index !== -1) {
+                    data = data.replace(targetRegex, replace);
+                    replaced = replaceFirst ? true : false;
+                }
+            }
+            /*
+             * target string may span two chunks
+             * so we need to keep track of the remaining data
+             */
+            remaining = data.slice(-target.length);
+            callback(null, data.slice(0, -target.length));
+        },
+        // Define the 'flush' function that is called when there are no more chunks to process
+        flush(callback) {
+            callback(null, remaining);
+        }
+    });
+    // Create a read stream to read data from the input file in smaller chunks
+    const readStream = createReadStream(filePath);
+    // Create a write stream to write data to a temporary output file
+    const writeStream = createWriteStream(filePath + '.tmp');
+
+    // Handle errors when reading from the input file
+    readStream.on('error', (err) => {
+        console.error(chalk.red(`Error reading file: ${err.message}`));
+        // Destroy the read stream to prevent a file descriptor leak
+        readStream.destroy();
+        writeStream.end();
+    });
+
+    // Handle errors when writing to the output file
+    writeStream.on('error', (err) => {
+        console.error(chalk.red(`Error writing file: ${err.message}`));
+        // Destroy the read stream to prevent a file descriptor leak
+        readStream.destroy();
+        writeStream.end();
+    });
+
+    readStream
+        .pipe(transformStream)
+        .pipe(writeStream)
+        .on('finish', () => {
+            // When all data has been processed and written, rename the temporary output file to overwrite the original input file
+            rename(filePath + '.tmp', filePath, (err) => {
+                if (err) {
+                    console.error(chalk.red(`Error renaming file: ${err.message}`));
+                } else {
+                    console.log(chalk.green('Replaced!'));
+                }
+            });
+            console.log('Replaced!');
+        });
 }
